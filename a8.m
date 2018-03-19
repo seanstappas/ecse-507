b054cd5c8d95b3dfea1acff75ce08b13ff93da28
Sean Stappas <seanstappas@gmail.com>
2018-03-19 14:18:13 -0400

    removed Fletcher-Reeves method

--- a8.m
+++ a8.m
@@ -22,8 +22,7 @@ disp("1. Gradient method");
 disp("2. Globalized Newton's method");
 disp("3. Globalized BFGS method");
 disp("4. Globalized inexact Newton's method");
-disp("5. Fletcher-Reeves method");
-disp("6. All methods (for comparison)");
+disp("5. All methods (for comparison)");
 method_index = input('Choose the minimization method. ');
 switch method_index
     case 1
@@ -43,8 +42,6 @@ switch method_index
         [x, k, x_ks] = newton_inexact(f, x0, epsilon);
         display_solution(f, x, k, x_ks, x_range);
     case 5
-        disp("Fletcher-Reeves method");
-    case 6
         disp("----------------------------------")
         disp("Gradient method");
         [x, k, x_ks] = gradient_method(f, x0, epsilon);
@@ -61,9 +58,6 @@ switch method_index
         disp("Globalized inexact Newton's method");
         [x, k, x_ks] = newton_inexact(f, x0, epsilon);
         display_solution(f, x, k, x_ks, x_range);
-        disp("----------------------------------")
-        disp("Fletcher-Reeves method");
-        disp("----------------------------------")
     otherwise
         disp('Invalid minimization method.');
 end
