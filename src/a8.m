55b743ec4e99ebbcec36610a6c6d21b7ff6f852c
Sean Stappas <seanstappas@gmail.com>
2018-03-22 12:05:22 -0400

    Created a8 tex and modified displayed strings

--- src/a8.m
+++ src/a8.m
@@ -1,66 +1,66 @@
 epsilon = 1e-6;
 
-disp("1. Rosenbrock function");
-disp("2. Himmelblau function");
-function_index = input('Choose the function to be minimized. ');
+disp('1. Rosenbrock function');
+disp('2. Himmelblau function');
+function_index = input('Choose the function to be minimized: ');
 switch function_index
     case 1
-        disp("==================================");
-        disp("       Rosenbrock function        ");
-        disp("==================================");
+        disp('==================================');
+        disp('       Rosenbrock function        ');
+        disp('==================================');
         f = f_rosenbrock();
         x0 = [-1.2; 1];
         x_range = [-1.5, 1.5, -0.5, 2];
     case 2
-        disp("==================================");
-        disp("       Himmelblau function        ");
-        disp("==================================");
+        disp('==================================');
+        disp('       Himmelblau function        ');
+        disp('==================================');
         f = f_himmelblau();
         x0 = [4; 4];
         x_range = [-6, 6, -6, 6];
     otherwise
-        disp("Invalid function.");
+        disp('Invalid function.');
         return;
 end
 
-disp("1. Gradient method");
-disp("2. Globalized Newton's method");
-disp("3. Globalized BFGS method");
-disp("4. Globalized inexact Newton's method");
-disp("5. All methods (for comparison)");
-method_index = input('Choose the minimization method. ');
+disp('1. Gradient method');
+disp('2. Globalized Newton''s method');
+disp('3. Globalized BFGS method');
+disp('4. Globalized inexact Newton''s method');
+disp('5. All methods (for comparison)');
+method_index = input('Choose the minimization method: ');
 switch method_index
     case 1
-        disp("Gradient method");
+        disp('Gradient method');
         [x, k] = gradient_method(f, x0, epsilon);
         display_solution(f, x, k);
     case 2
-        disp("Globalized Newton's method");
+        disp('Globalized Newton''s method');
         [x, k] = newton_global(f, x0, epsilon);
         display_solution(f, x, k);
     case 3
-        disp("Globalized BFGS method");
+        disp('Globalized BFGS method');
         [x, k] = bfgs_global(f, x0, epsilon);
         display_solution(f, x, k);
     case 4
-        disp("Globalized inexact Newton's method");
+        disp('Globalized inexact Newton''s method');
         [x, k] = newton_inexact(f, x0, epsilon);
         display_solution(f, x, k);
     case 5
-        disp("----------------------------------")
-        disp("Gradient method");
+        disp('----------------------------------')
+        disp('Gradient method');
         [x, k] = gradient_method(f, x0, epsilon);
         display_solution(f, x, k);
-        disp("----------------------------------")
-        disp("Globalized Newton's method");
+        disp('----------------------------------')
+        disp('Globalized Newton''s method');
         [x, k] = newton_global(f, x0, epsilon);
         display_solution(f, x, k);
-        disp("----------------------------------")
-        disp("Globalized BFGS method");
+        disp('----------------------------------')
+        disp('Globalized BFGS method');
         [x, k] = bfgs_global(f, x0, epsilon);
         display_solution(f, x, k);
-        disp("----------------------------------")
-        disp("Globalized inexact Newton's method");
+        disp('----------------------------------')
+        disp('Globalized inexact Newton''s method');
         [x, k] = newton_inexact(f, x0, epsilon);
         display_solution(f, x, k);
     otherwise
--- src/display_solution.m
+++ src/display_solution.m
@@ -1,7 +1,7 @@
 function display_solution(f, x, k)
-disp("Solution x:");
+disp('Solution x:');
 disp(x);
-disp("Solution f(x):");
+disp('Solution f(x):');
 disp(f(x));
-disp("Number of iterations:");
+disp('Number of iterations:');
 disp(k);
